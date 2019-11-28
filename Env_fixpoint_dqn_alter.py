c50ae7d5ae49be5cac2472b489eef3d4ac02d31e
simonhyx <simonhyx@gmail.com>
2019-11-27 19:57:35 -0500

    update

--- Env_fixpoint_dqn_alter.py
+++ Env_fixpoint_dqn_alter.py
@@ -307,8 +307,8 @@ class controlEnv():
                 ep21, ep22, ep23, ep24, ep25, ep26, ep27, ep28, ep29, ep30, 
                 ep31, ep32, ep33, ep34, ep35, ep36, ep37, ep38, ep39, ep40, 
                 ep41])
-        index = np.where(S <0)[0]
-        S[index] = 0
+        index = np.where(x <0)[0]
+        x[index] = 0
         
         if nodeIndex is not None:
             S[nodeIndex] = nodeVal
