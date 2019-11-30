699cc5a22134e3bfd8970dc7737ae681ff74bc66
simonhyx <simonhyx@gmail.com>
2019-11-30 13:26:43 -0500

    ddpg update

--- Env_ddpg_multipleStates.py
+++ Env_ddpg_multipleStates.py
@@ -86,6 +86,8 @@ class controlEnv():
             self.currentState = self.getAllStates()
             
         print(self.currentState.values)
+        index = np.where(self.currentState <0)[0]
+        self.currentState.iloc[index] = 0
         observation = np.log10(self.currentState.values.reshape(1, self.currentState.shape[0]*self.currentState.shape[1] ) +1)
         #action_obs = np.zeros(self.action_space.n)
         #action_obs[np.array(self.action_list)] = 1
