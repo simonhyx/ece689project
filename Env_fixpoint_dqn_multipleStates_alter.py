4b65b667d8afd696677c42d90fd2e78cb5993176
simonhyx <simonhyx@gmail.com>
2019-12-07 12:00:39 -0500

    dqn fixpoint

--- Env_fixpoint_dqn_multipleStates_alter.py
+++ Env_fixpoint_dqn_multipleStates_alter.py
@@ -449,7 +449,7 @@ class controlEnv():
         
         index = np.where(self.targetState > 0 )[0]
         threshold = np.log10(self.targetState[index]+1)
-        
+        print(observation.shape)
         observation = np.concatenate((observation, np.array([[threshold]])), axis = 1 )
         
         
